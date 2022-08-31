af264f620a35755b1f15423c5241c6f6572ea545
turan çabuk <turan6677@gmail.com>
2022-08-31 20:39:29 +0300

    VNCoreMLModel has been created.

--- MachineLearningImageRecognition/ViewController.swift
+++ MachineLearningImageRecognition/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
@@ -13,6 +15,8 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
+    
+    var chosenImage = CIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,10 +30,30 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
-        self.dismiss(animated: true, completion: nil
+        self.dismiss(animated: true, completion: nil)
         
         
-        )
+        if let ciImage = CIImage(image: imageView.image!){
+            chosenImage = ciImage
+        }
+        
+        recognizeImage(image: chosenImage)
+    
+    }
+    func recognizeImage( image : CIImage){
+        
+        if let model = try? VNCoreMLModel(for: MobileNetV2().model){
+            
+            let request = VNCoreMLRequest(model: model) { vnrequest, error in
+                
+                if let results = vnrequest.results as? [VNClassificationObservation] {
+                    if results.count > 0{
+                        
+                        let topResult = results.first
+                    }
+                }
+            }
+        }
     }
     
 }
